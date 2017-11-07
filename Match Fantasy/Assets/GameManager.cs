b2c4f502b7085992f40113ee0b831dec8c524310
Zalron <33308793+Zalron@users.noreply.github.com>
2017-11-07 18:01:25 +1100

    wprked on more of the code

--- Match Fantasy/Assets/GameManager.cs
+++ Match Fantasy/Assets/GameManager.cs
@@ -17,11 +17,21 @@ public class GameManager : MonoBehaviour
     Image tile1 = null;
     Image tile2 = null;
     public Image[] image;
+    List<GameObject> tileBank = new List<GameObject>();
     static int rows = 8;
     static int cols = 8;
     Tile[,] tiles = new Tile[cols, rows];
-    void Start () // Use this for initialization
+    void Start() // Use this for initialization
     {
+        int numCopies = (rows * cols) / 3;
+        for (int i = 0; i < numCopies; i++)
+        {
+            for (int j = 0; j < tiles.Length; j++)
+            {
+                GameObject o = (GameObject) Instantiate(tiles[j], new Vector3(-10, -10, 0), tiles[j].transform.rotation);
+            }
+        }
+        tileBank.Add(0);
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; r < cols; c++)
