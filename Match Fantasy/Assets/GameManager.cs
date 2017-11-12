b887391f41afc6c6f69cf37f65b9542f144ea8bd
Zalron <davidrlawson@optusnet.com.au>
2017-11-12 17:44:19 +1100

    trouble shooting again

--- Match Fantasy/Assets/GameManager.cs
+++ Match Fantasy/Assets/GameManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Tile
+public class Tile // used by the tiles to point to the same tiles when matching
 {
     public GameObject tileObj;
     public string type;
@@ -13,15 +13,15 @@ public class Tile
 }
 public class GameManager : MonoBehaviour
 {
-    GameObject tile1 = null;
-    GameObject tile2 = null;
-	public GameObject[] tile;
+    GameObject tile1 = null; // variales used to keep the first clicked tile
+    GameObject tile2 = null; // variable used to keep the second clicked tile
+	public GameObject[] tile; // array called tile
     List<GameObject> tileBank = new List<GameObject>();
-    static int rows = 8;
-    static int cols = 8;
+	static int rows = 8; // how many rows in the match 3 (tutorial code)
+	static int cols = 8; // how many columns in the match 3 (tutorial code)
 	bool renewBoard = false;
-    Tile[,] tiles = new Tile[cols, rows]; // A Array of objects for the tiles for the match 3
-	void ShuffleList()
+	Tile[,] tiles = new Tile[cols, rows]; // A Array of objects for the tiles for the match 3 (tutorial code)
+	void ShuffleList() // shuffles the list of tiles (tutorial code)
 	{
 		System.Random rand = new System.Random ();
 		int r = tileBank.Count;
@@ -36,8 +36,8 @@ public class GameManager : MonoBehaviour
 	}
     void Start() // Use this for initialization
     {
-        int numCopies = (rows * cols);
-        for (int i = 0; i < numCopies; i++)
+        int numCopies = (rows * cols)/3;
+        for (int i = 0; i < numCopies; i++) // creating copies so that we don't run out of copies 
         {
             for (int j = 0; j < tile.Length; j++)
             {
@@ -69,35 +69,35 @@ public class GameManager : MonoBehaviour
 	void Update () // Update is called once per frame
     {
 		CheckGrid ();
-		if (Input.GetMouseButtonDown (0)) 
+		if (Input.GetMouseButtonDown (0)) // casting a ray into the scene if the mouse was clicked for a hit on the first tile 
 		{
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit2D hit = Physics2D.GetRayIntersection (ray, 1000);
-			if (hit) 
+			if (hit) // makes the hit tile in the variable
 			{
 				tile1 = hit.collider.gameObject;
 			}
 		}
-		else if(Input.GetMouseButtonUp(0)&& tile1)
+		else if(Input.GetMouseButtonUp(0)&& tile1) // casting a ray into the scene if the mouse was clicked for a hit on the second tile 
 		{
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit2D hit = Physics2D.GetRayIntersection (ray, 1000);
-			if (hit) 
+			if (hit) // makes the hit tile in the variable tile2
 			{
 				tile2 = hit.collider.gameObject;
 			}
-			if (tile1 && tile2) 
+			if (tile1 && tile2) // runs if their are two tiles clicked
 			{
 				int horzDist = (int)Mathf.Abs (tile1.transform.position.x - tile2.transform.position.x);
 				int vertDist = (int)Mathf.Abs (tile1.transform.position.y - tile2.transform.position.y);
-				if (horzDist == 1 ^ vertDist == 1) 
+				if (horzDist == 1 & vertDist == 0 || horzDist == 0 & vertDist == 1) // swaps the position of the tow hit tiles
 				{
 					Tile temp = tiles [(int)tile1.transform.position.x, (int)tile1.transform.position.y];
 					tiles [(int)tile1.transform.position.x, (int)tile1.transform.position.y] = tiles [(int)tile2.transform.position.x, (int)tile2.transform.position.y];
 					tiles [(int)tile2.transform.position.x, (int)tile2.transform.position.y] = temp; // code block is keeping track of the temp positions of the tiles in memory
 					Vector3 tempPos = tile1.transform.position;
 					tile1.transform.position = tile2.transform.position;
-					tile2.transform.position = tempPos;
+					tile2.transform.position = tempPos; // moves the second tile to the first position
 					tile1 = null; // Resets the touched tile variable
 					tile2 = null; // Resets the touched tile variable
 				} 
@@ -108,7 +108,7 @@ public class GameManager : MonoBehaviour
 			}
 		}
 	}
-	void CheckGrid()
+	void CheckGrid() // function for checking the grid for matches 
 	{
 		int counter = 1;
 		for(int r = 0; r < rows; r++) // checks for coloums
@@ -140,9 +140,9 @@ public class GameManager : MonoBehaviour
 						{
 							tiles [c-2, r].tileObj.SetActive (false);
 						}
-						tiles [c, r] = null;
-						tiles [c - 1, r] = null;
-						tiles [c - 2, r] = null;
+						tiles [c, r] = null; //resets first tile reference
+						tiles [c - 1, r] = null; //resets second tile reference
+						tiles [c - 2, r] = null; //resets third tile reference
 						renewBoard = true;
 					}
 				}
@@ -184,13 +184,17 @@ public class GameManager : MonoBehaviour
 				}
 			}
 		}
-		if(renewBoard)
+		if(renewBoard) // renews the board
 		{
 			RenewGrid ();
 			renewBoard = false;
 		}
 	}
-	void RenewGrid()
+	void MoveCharacterOnGrid () // moves the character on the grid to the matching colour
+	{
+		
+	}
+	void RenewGrid() // renews the grid after a pair of three is taked away
 	{
 		bool anyMoved = false;
 		ShuffleList ();
